5248c23c7326f1525b61c703b96758ca465a8cc0
Simone Gentili <sensorario@gmail.com>
2023-04-12 23:40:57 +0200

    feat: react 001

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Page() {
+    return <React.Fragment>
+        <h1>Se questo video ti è piaciuto, metti un like!</h1>
+        <h2>Seguimi per non perderti i prossimi video!!!</h2>
+    </React.Fragment>
+}
